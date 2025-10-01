e2493f221832780af9fbb770e659737afd8a76fd
v-khushalani <vkhushalani0001@gmail.com>
2025-10-01 23:02:51 +0530

    Update EnhancedDashboard.tsx

--- src/pages/EnhancedDashboard.tsx
+++ src/pages/EnhancedDashboard.tsx
@@ -99,9 +99,6 @@ const EnhancedDashboard = () => {
     <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 overflow-hidden">
       <Header />
       
-      <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
-      <Header />
-      
       {/* Added proper spacing to avoid navbar overlap */}
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl pt-24">
         {/* Welcome Section */}

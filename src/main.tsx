f60a69a3b048008b2974caded0fa5079ec11adde
v-khushalani <vkhushalani0001@gmail.com>
2025-10-12 11:07:10 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ if (!container) {
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
